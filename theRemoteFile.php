0ba7b56cb668dd7f2864ebfac96ddbc8e8f48b9a
Jason Jersey <icryptic@users.noreply.github.com>
2014-09-11 23:44:39 -0600

    Update theRemoteFile.php

--- theRemoteFile.php
+++ theRemoteFile.php
@@ -4,7 +4,7 @@
  * @file: theRemoteFile.php
  * @date: Sept, 09, 2014
  * @auth: Jason Jersey
- * @vers: v 1.1.3
+ * @vers: v 1.1.4
  */
  
 /* THERE IS NO WARRANTY FOR THIS SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT 
@@ -28,69 +28,19 @@ if (stripos($_SERVER['REQUEST_URI'], "?key=$key?secret=$secret")){
    /* Ignore user aborts and allow the script to run forever */
    if(function_exists('ignore_user_abort')) {
       ignore_user_abort(true);
-      set_time_limit(60);
+      set_time_limit(30);
    }
 
    /* checks if exec is enabled */
    if(function_exists('exec')) {
 
-   /* set variables */
-   $check_mysql = exec('/etc/init.d/mysql restart');
-   $check_postgresql = exec('/etc/init.d/postgresql restart');
-   $check_apached = exec('/etc/init.d/apache2 restart');
-   $check_apache = exec('/sbin/service apache2 restart');
-   $check_httpd = exec('/etc/init.d/httpd restart');
-   $check_sshd = exec('/etc/init.d/sshd restart');
-   $check_ssh = exec('/etc/init.d/ssh restart');
-   $check_ssh_rcd = exec('/etc/rc.d/sshd restart');
-
-   /* check mysql */
-   if ($check_mysql !== false) {
-      exec('/etc/init.d/mysql restart');
-      echo "/etc/init.d/mysql restart<br>";
-   }
-
-   /* check postgresql  */
-   if ($check_postgresql !== false) {
-      exec('/etc/init.d/postgresql restart');
-      echo "/etc/init.d/postgresql restart<br>";
-   }
-
-   /* check apache */
-   if ($check_apached !== false) {
-      exec('/etc/init.d/apache2 restart');
-      echo "/etc/init.d/apache2 restart<br>";
-   }
-
-   /* check apache alt */
-   if ($check_apache !== false) {
-      exec('/sbin/service apache2 restart');
-      echo "/sbin/service apache2 restart<br>";
-   }
-
-   /* check httpd */
-   if ($check_httpd !== false) {
-      exec('/etc/init.d/httpd restart');
-      echo "/etc/init.d/httpd restart<br>";
-   }
-
-   /* check sshd */
-   if ($check_sshd !== false) {
-      exec('/etc/init.d/sshd restart');
-      echo "/etc/init.d/sshd restart<br>";
-   }
-
-   /* check ssh */
-   if ($check_ssh !== false) {
-      exec('/etc/init.d/ssh restart');
-      echo "/etc/init.d/ssh restart<br>";
-   }
-
-   /* check ssh rc.d */
-   if ($check_ssh_rcd !== false) {
-      exec('/etc/rc.d/sshd restart');
-      echo "/etc/rc.d/sshd restart<br>";
-   }
+   /* restart services (linux) */
+   exec('/etc/init.d/mysql restart');
+   exec('/etc/init.d/postgresql restart');
+   exec('/etc/init.d/apache2 restart');
+   exec('/etc/init.d/httpd restart');
+   exec('/etc/init.d/sshd restart');
+   exec('/etc/init.d/ssh restart');
 
    }else{
       echo "php exec is disabled";
